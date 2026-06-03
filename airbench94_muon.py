6960f0f1c27c2fef6d804905091c2a1316dd79b1
VishyTheFishy <69478716+VishyTheFishy@users.noreply.github.com>
2026-06-02 23:07:00 -0700

    Update airbench94_muon.py

--- airbench94_muon.py
+++ airbench94_muon.py
@@ -31,7 +31,51 @@ torch.backends.cudnn.benchmark = True
 #############################################
 #               Muon optimizer              #
 #############################################
-
+"""
+def zeropower_via_newtonschulz5(G, steps=3, eps=1e-7):
+    """
+    Newton-Schulz iteration to compute the zeroth power / orthogonalization of G. We opt to use a
+    quintic iteration whose coefficients are selected to maximize the slope at zero. For the purpose
+    of minimizing steps, it turns out to be empirically effective to keep increasing the slope at
+    zero even beyond the point where the iteration no longer converges all the way to one everywhere
+    on the interval. This iteration therefore does not produce UV^T but rather something like US'V^T
+    where S' is diagonal with S_{ii}' \sim Uniform(0.5, 1.5), which turns out not to hurt model
+    performance at all relative to UV^T, where USV^T = G is the SVD.
+    """
+    assert len(G.shape) == 2
+    a, b, c = (3, -16/5, 6/5)#(3.4445, -4.7750,  2.0315)#
+    X = G.bfloat16()
+    X = X/(X.norm() + eps) # ensure top singular value <= 1
+    if G.size(0) > G.size(1):
+        X = X.T
+    for _ in range(steps):
+        A = X @ X.T
+        B = b * A + c * A @ A
+        X = a * X + B @ X
+    if G.size(0) > G.size(1):
+        X = X.T
+    return X
+    
+def targeted_newtonschulz5(G, steps:int = 7, tau: float = 0):
+    tau = TAU
+    assert G.ndim >= 2
+    X = G.bfloat16()
+    if G.size(-1) > G.size(-2):
+        X = X.mT
+    n = X.size(-1)
+    I = torch.eye(n, dtype=X.dtype, device=X.device) 
+    M = X.mT @ X - tau**2 * I
+    signedM = zeropower_via_newtonschulz5(M, steps)
+    projBot = 0.5 * (I - signedM)
+    projTop = 0.5 * (I + signedM)
+    nsX = zeropower_via_newtonschulz5(X, steps)
+    nsBot = nsX @ projBot + X @ projTop
+    nsTop = X @ projBot + nsX @ projTop
+    if G.size(-1) > G.size(-2):
+        nsBot = nsBot.mT
+        nsTop = nsTop.mT
+    return nsBot #(nsBot, nsTop)
+"""
 @torch.compile
 def zeropower_via_newtonschulz5(G, steps=3, eps=1e-7):
     assert len(G.shape) == 2
